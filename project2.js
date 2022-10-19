6d3348085f33c8b62f292c279fa4fd5e470ef81c
Deepak Patil <patildeepak2905@gmail.com>
2022-10-19 22:16:34 +0530

    Initial commit

---
+++ project2.js
@@ -0,0 +1,25 @@
+const endDate = "08 October 2022 11:17 AM"
+
+document.getElementById("end-Date").innerText=endDate;
+const inputs = document.getElementsByTagName("input")
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end-now)/1000;
+
+    if(diff < 0) return;
+    
+    inputs[0].value = (Math.floor(diff / 3600 / 24));
+    inputs[1].value = Math.floor(diff/3600)%24;
+    inputs[2].value = Math.floor(diff/60)%60;
+    inputs[3].value = Math.floor(diff)%60;
+    
+}
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },1000
+)
